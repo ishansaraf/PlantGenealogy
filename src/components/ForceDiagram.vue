09bd1d7f8e7abc01d7a23d37ecb5e1cae3d8ea8e
Ishan Saraf <sarafi@rose-hulman.edu>
2019-02-17 21:41:26 -0500

    Cards v1

--- src/components/ForceDiagram.vue
+++ src/components/ForceDiagram.vue
@@ -3,23 +3,32 @@
     <h1>D3 is confusing - But here's a force layout graph</h1>
     <label for="colorby"> Color By: </label>
     <select id="colorby" @change="colorStrains()" v-model="selected">
-    <option value="type">Strain Type</option>
-    <option value="effect">Strongest Effect</option>
-    <option value="neg">Biggest Negative</option>
-    <option value="med">Strongest Medical Property</option>
-  </select>
+      <option value="type">Strain Type</option>
+      <option value="effect">Strongest Effect</option>
+      <option value="neg">Biggest Negative</option>
+      <option value="med">Strongest Medical Property</option>
+    </select>
+    <node-info :node_info="current_node"></node-info>
   </div>
 </template>
 
 <script>
 import * as d3 from 'd3';
 import strains from '../../Data/strains_formatted.json';
+import NodeInfo from './NodeInfo.vue';
 
 export default {
   name: 'ForceDiagram',
   props: {},
+  components: {
+    'node-info': NodeInfo,
+  },
   data() {
     return {
+      current_node: {
+        name: '',
+        url: '',
+      },
       selected: 'type',
       keyArea: null,
     };
@@ -133,15 +142,13 @@ export default {
         )
         .on('click', doClick);
 
-      const keyGroup = g
-        .append('g')
-        .attr('class', 'key');
+      const keyGroup = g.append('g').attr('class', 'key');
 
-      const key = keyGroup
-        .selectAll('g');
+      const key = keyGroup.selectAll('g');
       this.keyArea = key;
 
-      keyGroup.append('rect') // Nice rectangle around the key
+      keyGroup
+        .append('rect') // Nice rectangle around the key
         .attr('class', 'keyBox')
         .attr('width', 140)
         .attr('height', 50)
@@ -150,12 +157,10 @@ export default {
         .attr('fill', '#ffffff')
         .attr('stroke', '#000000');
 
-
       // colorStrains('type');
       // colorStrains('effect');
       this.colorStrains('neg');
       // colorStrains('med')
-
 
       // eslint-disable-next-line
       // const labels = node
@@ -163,7 +168,6 @@ export default {
       //  .text(d => d.name)
       //  .attr('x', 6)
       //  .attr('y', 3);
-
 
       node.append('title').text(d => d.name);
 
@@ -194,6 +198,7 @@ export default {
 
       // https://stackoverflow.com/a/30714153
       let selected;
+      const self = this;
       function doClick() {
         if (!selected) {
           selected = this;
@@ -210,6 +215,7 @@ export default {
         const strainInfo = strains.info[strainId];
         // eslint-disable-next-line
         console.log(strainInfo);
+        self.current_node = strainInfo;
       }
       // Handles alpha forces to deal with node select and drag
       function dragStart(d) {
@@ -306,17 +312,20 @@ export default {
       }
       d3.selectAll('circle').attr('fill', d => color(d[`cat_${attrName}`]));
       d3.selectAll('.keyRepr').remove();
-      const tmpGroups = this.keyArea.data(values)
+      const tmpGroups = this.keyArea
+        .data(values)
         .enter()
         .append('g')
         .attr('class', 'keyRepr');
-      tmpGroups.append('rect')
+      tmpGroups
+        .append('rect')
         .attr('width', 20)
         .attr('height', 20)
         .attr('x', 10)
         .attr('y', d => 30 * d.pos + 10)
         .attr('fill', d => d.color);
-      tmpGroups.append('text')
+      tmpGroups
+        .append('text')
         .text(d => d.label)
         .attr('x', 40)
         .attr('y', d => 30 * d.pos + 25);
---
+++ src/components/NodeInfo.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="node_info">
+    <h1>
+      <a :href=node_info.url>
+        {{ node_info.name }}
+      </a>
+    </h1>
+    <p>{{ node_info.desc }}</p>
+    <!-- <ul><li for parent in parents></li></ul> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NodeInfo',
+  props: {
+    node_info: {},
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#node_info {
+  width: 70%;
+  padding: 10px;
+  text-align: start;
+  margin: 25px auto;
+  border: 1px solid lawngreen;
+}
+
+a {
+  color: lawngreen;
+}
+
+.dot {
+  float: right;
+  height: 25px;
+  width: 25px;
+  background-color: orange;
+  display: inline-block;
+  border-radius: 50%;
+}
+</style>
